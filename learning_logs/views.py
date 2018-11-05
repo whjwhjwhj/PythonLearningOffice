9ff234f0bfd0ec3099b56ec06cd7b5ce4ec6fee0
whjwhjwhj <wei_hj@aliyun.com>
2018-11-05 17:05:26 +0800

    2018/11/5  添加主题

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.http import  HttpResponseRedirect
+from django.urls import reverse
 from .models import Topic
+from .forms import TopicForm
 
 # Create your views here.
 def index(request):
@@ -18,5 +21,26 @@ def topic(request,topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """添加新主题"""
+    if request.method != 'POST':
+        #位提交数据，创建一个新表单
+        form = TopicForm()
+    else :
+        #POST递交新的数据，对数据进行处理
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    context = {'form':form}
+    return render(request,'learning_logs/new_topic.html', context)
+
+
+
+
+
+
+
 
 

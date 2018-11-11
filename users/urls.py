8ecf9a8f9502ff8e01ea7ca4ff6fd91ee54007bc
whjwhjwhj <wei_hj@aliyun.com>
2018-11-11 22:48:00 +0800

    登陆设置

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
 """为应用程序users定义URL模式"""
-from django.conf.urls import  url
-from django.contrib.auth.views import login
+from django.conf.urls import url
+from django.contrib.auth.views import LoginView
 
 from . import views
+app_name = 'users'
 
 urlpatterns = [
     #登陆页面
-    url(r'^login/$', login, {'template_name': 'users/login.html'},
-        name='login'),
+    url(r'^login/$', LoginView.as_view(template_name='users/login.html'), name='login'),
 ]

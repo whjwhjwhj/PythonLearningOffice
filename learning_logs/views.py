91d7760cc1ec01a01deebb65ac6dcfb07350a6a3
whjwhjwhj <wei_hj@aliyun.com>
2018-11-13 15:38:10 +0800

    2018/11/13  项目开发完成

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -3,25 +3,33 @@ from django.http import  HttpResponseRedirect
 from django.urls import reverse
 from .models import Topic, Entry
 from .forms import TopicForm, Entryform
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect, Http404
 
 # Create your views here.
 def index(request):
     """学习笔记的主页"""
     return render(request, 'learning_logs/index.html')
 
+@login_required
 def topics(request):
     """显示所有主题"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
-def topic(request,topic_id):
+@login_required
+def topic(request, topic_id):
     """显示单个主题机器所有的条目"""
     topic = Topic.objects.get(id=topic_id)
+    #确认请求的主题属于当前用户
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """添加新主题"""
     if request.method != 'POST':
@@ -31,11 +39,15 @@ def new_topic(request):
         #POST递交新的数据，对数据进行处理
         form = TopicForm(request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return HttpResponseRedirect(reverse('learning_logs:topics'))
+
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """在特点主题中添加新条目"""
     topic = Topic.objects.get(id=topic_id)
@@ -54,10 +66,14 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """编辑既有条目"""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method != 'POST':
         #初次请求，使用当前条目填充表单

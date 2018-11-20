0793d2eb0f4421a2eae7e1b126773192bfbe3078
whjwhjwhj <wei_hj@aliyun.com>
2018-11-20 10:43:09 +0800

    2018/11/20 部署文件完成。

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -136,4 +136,22 @@ LOGIN_URL = '/users/login/'
 BOOTSTRAP4 = {'include _jquery': True}
 #STATICFILES_DIRS = (os.path.join('static'),)
 
+#heroku设置
+if os.getcwd() =='/app':
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(default='postgres://localhost')
+    }
+    #让request.is_secure()承认-Forwarded-Proto头
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    #支持所有的主机头（host header)
+    ALLOWED_HOSTS = ['*']
+
+    #静态资产配置
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static')
+    )
 
--- learning_log/wsgi.py
+++ learning_log/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
 
 import os
-
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learning_log.settings')

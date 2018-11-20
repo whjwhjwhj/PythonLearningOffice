46e6a1e19bbda7b9b25cbb14ba5aaf1e109ae6c1
whjwhjwhj <wei_hj@aliyun.com>
2018-11-20 15:27:13 +0800

    2018/11/20 修改 settings.py。

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -149,8 +149,9 @@ if os.getcwd() =='/app':
     ALLOWED_HOSTS = ['*']
 
     #静态资产配置
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = 'staticfiles'
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static')
     )

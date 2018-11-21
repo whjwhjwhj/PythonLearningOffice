478a51aaf94528f859c930ea3b3967ddab67bb12
whjwhjwhj <wei_hj@aliyun.com>
2018-11-21 17:11:50 +0800

    2018/11/21 刷新3.7.0到3.7.1。

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -153,11 +153,11 @@ if os.getcwd() =='/app':
     ALLOWED_HOSTS = ['*']
 
 #静态资产配置
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
-)
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static')
+# )
 
 django_heroku.settings(locals())

4f028d70df888e2e4ee12ff567707ab3fb7d9bd0
whjwhjwhj <wei_hj@aliyun.com>
2018-11-25 12:23:12 +0800

    2018/11/21 刷新3.7.0到3.7.1。

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,9 +137,6 @@ BOOTSTRAP4 = {'include _jquery': True}
 #STATICFILES_DIRS = (os.path.join('static'),)
 
 #heroku设置
-
-
-
 if os.getcwd() =='/app':
     import dj_database_url
     DATABASES = {
@@ -152,12 +148,10 @@ if os.getcwd() =='/app':
     #支持所有的主机头（host header)
     ALLOWED_HOSTS = ['*']
 
-#静态资产配置
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+    #静态资产配置
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static')
+    )
 
-#django_heroku.settings(locals())
